f36c0b6062788ead08555b1874e1fcd49b6fca82
venkatacharan.e <venkatacharan.e@acsicorp.com>
2023-07-03 16:21:48 +0530

    added br
    Signed-off-by: venkatacharan.e <venkatacharan.e@acsicorp.com>

---
+++ src/custom-webpack.config.js
@@ -0,0 +1,24 @@
+const CompressionPlugin = require("compression-webpack-plugin");
+
+module.exports = {
+  plugins: [new CompressionPlugin()],
+};
+const zlib = require("zlib");
+
+module.exports = {
+  plugins: [
+    new CompressionPlugin({
+      filename: "[path][base].br",
+      algorithm: "brotliCompress",
+      test: /\.(js|css|html|svg)$/,
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+        },
+      },
+      threshold: 10240,
+      minRatio: 0.8,
+      deleteOriginalAssets: true,
+    }),
+  ],
+};
